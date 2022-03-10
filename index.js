e093a3eeec32655cbbde701d80baa3819f88461d
July Agbiaowei <aaronjulya@gmail.com>
2022-03-10 17:04:19 +0900

    Update index.js (add Animation to pressed button)

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ for (var i=0; i<clickedButton; i++) {
 
 var buttonInnerHTML = this.innerHTML;
 makeSound(buttonInnerHTML);
-
+buttonAnimation(buttonInnerHTML);
   });
 }
 
 // Detecting keyboard pressed
 document.addEventListener("keypress", function(event){
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -63,11 +64,14 @@ function makeSound(key) {
     default: console.log(buttonInnerHTML);
   }
 
-
-
 }
 
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+ currentKey);
+  activeButton.classList.add("pressed");
 
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
 
-
-// this.style.color= "white";
